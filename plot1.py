ee581ff25c29233a3819b811c76350e68810fb6a
AZKAR <azkarsidik@gmail.com>
2024-11-20 22:54:19 +0800

    Add files via upload

---
+++ plot1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x_axis=[5,10,15,20,25]
+y_axis=[689,765,567,234,980]
+plt.plot(x_axis,y_axis)
+plt.title("Pressure Chart")
+plt.xlabel("weeks")
+plt.ylabel("Pressure")
+plt.show()
